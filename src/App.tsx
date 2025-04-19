8dedb945e9b5637c7a97a05db7a556d74d8e7d13
Sumbilon-817 <olympiosumbilon17@gmail.com>
2025-04-19 19:12:27 +0800

    WIP: Add supabase connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css'
+import { useEffect } from 'react'
+import {supabase} from './supabaseClient'
 
 function App() {
+  useEffect(() => {
+    const testConnection = async () => {
+      const { data, error } = await supabase.auth.getSession()
+
+      if (error) {
+        console.error('❌ Supabase Error:', error.message)
+      } else {
+        console.log('✅ Supabase connected! Session data:', data)
+      }
+    }
+
+    testConnection()
+  }, [])
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-green-500 text-white">
-      <h1 className="text-4xl font-bold">🎉 Tailwind is Working Network Test!</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Login Page</h1>
     </div>
-  );
+  )
 }
 
 export default App
